47f3d4488e4be81dfcca0f3f0aeafbfd2e481f2b
Quiint <quiint.gh@gmail.com>
2021-09-22 19:03:18 +0000

    :truck: Rename base.html to index.html

--- project/urls.py
+++ project/urls.py
@@ -19,7 +19,7 @@ from django.urls import path
 from django.views.generic import TemplateView
 
 urlpatterns = [
-    path("", TemplateView.as_view(template_name="base.html")),
+    path("", TemplateView.as_view(template_name="index.html")),
 ]
 
 if settings.DEBUG:
